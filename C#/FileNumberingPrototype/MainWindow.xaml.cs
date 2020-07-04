934d5d9ca4ac78298ac147fd453df2e246306429
Milos Dimcic <milosdimcic@yahoo.com>
2020-07-04 16:22:49 +0200

    200704_Milos

--- C#/FileNumberingPrototype/MainWindow.xaml.cs
+++ C#/FileNumberingPrototype/MainWindow.xaml.cs
@@ -12,13 +12,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
 
 namespace FileNumberingPrototype
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
@@ -32,7 +34,51 @@ namespace FileNumberingPrototype
 
         private void UserOnClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("TEST");
+            MessageBox.Show("User Dialog");
+        }
+
+        private void ContentOnClick(object sender, RoutedEventArgs e)
+        {
+           // MessageBox.Show("Content Dialog");
+
+            //string path = Utility.GetFilePathXlsx("Get The Excel File");
+           string path = "E:\\triangle\\work\\work_ltd\\SupperLeggera\\development\\main\\super_leggera\\C#\\FileNumberingPrototype\\input\\Test5.xls";
+            //string path = "..\\..\\input\\Test5.xls";
+            // MessageBox.Show(path);
+
+            _Application excel = new _Excel.Application();
+            Workbook wb = excel.Workbooks.Open(path);
+            Worksheet ws= (Worksheet)wb.Worksheets[1];  
+            
+            Range excelRange = ws.UsedRange;
+            int rowCount = excelRange.Rows.Count;
+            int colCount = excelRange.Columns.Count;
+
+            string finalstring = "";
+            
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    for (int j = 1; j <= colCount; j++)
+                    {
+                        Range range = (ws.Cells[i, j] as Range);
+                        string cellValue = range.Value.ToString();
+ 
+                        //do anything
+                        finalstring += cellValue + "\n";
+                    }
+                }
+              
+            /*if(ws.Cells[i,j].Value2 !=null)
+            {
+                MessageBox.Show(ws.Cells[i,j].Value2);
+            }*/
+
+            wb.Close(false, null, null);
+            excel.Quit();
+
+
+            MessageBox.Show(finalstring);
+
         }
 
         private void Cb_VolumeSystem_SelectionChanged(object sender, SelectionChangedEventArgs e)
---
+++ C#/FileNumberingPrototype/Utility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileNumberingPrototype
+{
+    public static class Utility
+    {
+        public static string GetFilePathXlsx(string title)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Title = title;
+            saveFileDialog1.Filter              = "Excel Files | *.xls";
+            saveFileDialog1.DefaultExt          = "xls";
+            saveFileDialog1.RestoreDirectory    = true;
+          
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                   
+                    myStream.Close();
+                   
+                    return saveFileDialog1.FileName;
+                }
+                else
+                    return "";
+            }
+            else
+                return "";
+        }
+    }
+}
