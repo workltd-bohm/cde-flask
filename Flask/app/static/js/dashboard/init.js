947eaae7dfc293d03f2ab78de3a262d283d6b1c3
Filip Kraus <kraksorz@gmail.com>
2020-07-19 11:07:02 +0200

    frontend - dashboard start

---
+++ Flask/app/static/js/dashboard/init.js
@@ -0,0 +1,10 @@
+
+var DASHBOARD = $("#index-dashboard");
+
+var SVG_WIDTH = DASHBOARD;
+var SVG_HEIGHT = DASHBOARD.height();
+
+
+// SWG = d3.select("#SVG")
+//         .attr("width",DASHBOARD.width())
+//         .attr("height",SVG_HEIGHT);
