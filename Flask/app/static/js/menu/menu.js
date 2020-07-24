bbdc9f936c7c0b8de39ac53d4ee08bed44aa5914
Filip Kraus <kraksorz@gmail.com>
2020-07-24 21:50:00 +0200

    frontend - independet menu/activity slider

--- Flask/app/static/js/menu/menu.js
+++ Flask/app/static/js/menu/menu.js
@@ -1,2 +1,12 @@
 
 $("li[class='menu-link'][href*='/']").click(function(){location.href= $(this).attr("href");});
+
+$(".open").click(function(d){
+    $(this).parent().addClass("opend");
+    $(this).parent().removeClass("closed");
+});
+
+$(".close").click(function(d){
+    $(this).parent().removeClass("opend");
+    $(this).parent().addClass("closed");
+});
