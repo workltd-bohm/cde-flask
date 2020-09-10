af4215e9f2088bbd996e6c48460703ec30bc5fb9
Toms <toma.dimcic@gmail.com>
2020-09-10 17:21:54 +0200

    basic support for bids added

--- Flask/app/static/js/dashboard/market/market.js
+++ Flask/app/static/js/dashboard/market/market.js
@@ -25,7 +25,12 @@ function MarketOpen(type, data){
 //    }
 
     switch(type){
-        case "Bids": CreateBids; break;
+        case "Bids":
+            CreateBids(bid_new);
+            for (var obj of data){
+                setTimeout(function(){ CreateBids(bid_html, obj); }, ANIM_TICKET_WAIT);
+            }
+            break;
         case "Posts":
             CreatePosts(post_new);
             for (var obj of data){
@@ -40,6 +45,16 @@ function MarketOpen(type, data){
 //    }
 }
 
+bid_html = "<div class=\"ticket-body\" onclick=\"return;\">" +
+                "<h3>BID TEMPLATE</h3>" +
+                "<div class=\"ticket-box\">" +
+                    "<h4>cde</h4>" +
+                "</div>" +
+            "</div>"
+bid_new = "<div class=\"ticket-body\" onclick=\"GetAllPost();\">" +
+            "<h3>BROWSE POSTS</h3>" +
+            "<i class=\"material-icons w3-xxxlarge\">play_circle_filled</i>" +
+        "</div>"
 
 post_new = "<div class=\"ticket-body\" onclick=\"AddPost();\">" +
                 "<i class=\"material-icons w3-xxxlarge\">add_circle</i>" +
@@ -88,6 +103,31 @@ function CreatePosts(html, data=null){
     }
 
     console.log(newDiv);
+
+    var tmp = {};
+    tmp.body =  MARKET.append("div")
+        .attr("class","ticket")
+        .style("opacity", 0)
+
+    tmp.body.transition()
+        .ease("linear")
+        .duration(ANIM_TICKET_FADE)
+        .style("opacity", 100);
+
+    tmp.body.append("form")
+        .attr("class", "ticket-form")
+        .html(newDiv);
+
+    tmp.data = data;
+    g_market.childs.push(tmp);
+}
+
+function CreateBids(html, data=null){
+
+    var newDiv = bid_new;
+    if(data != null){
+        newDiv = bid_html;
+    }
 
     var tmp = {};
     tmp.body =  MARKET.append("div")
