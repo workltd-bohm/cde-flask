287e703f3484c9a14d792b1e0cdd6adf5598e25f
Tomislav Dimcic <tomislav.dimcic@tttech-auto.com>
2021-03-19 16:12:33 +0100

    email bug fix

--- Flask/app/controller/send_email.py
+++ Flask/app/controller/send_email.py
@@ -29,19 +29,23 @@ def send_an_email(username, email):
         server.starttls(context=context)
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
+        a = []
         for receiver in receiver_email:
-            message["To"] = receiver[0]
-            body = "<p>Hi %s,<br><br>If you want to confirm the account for: <br>" \
-                   "username: %s <br>" \
-                   "email: %s <br><br>" \
-                   "please click the following link: <br>" \
-                   "<br><a href=\"http://localhost/confirm_account?username=%s&email=%s\">Confirm</a> " \
-                   "</p>" % (receiver[1], username, email, username, email)
-            m = MIMEText(html1 + body + html2, "html")
-            message.attach(m)
-            server.sendmail(
-                sender_email, receiver[0], message.as_string()
-            )
-            print('message sent to: %s' %receiver[1])
+            a.append(receiver[0])
+        # for receiver in receiver_email:
+        message["To"] = ','.join(a)
+        # message["CC"] =
+        body = "<p>Hi %s,<br><br>If you want to confirm the account for: <br>" \
+               "username: %s <br>" \
+               "email: %s <br><br>" \
+               "please click the following link: <br>" \
+               "<br><a href=\"http://localhost/confirm_account?username=%s&email=%s\">Confirm</a> " \
+               "</p>" % ('admin', username, email, username, email)
+        m = MIMEText(html1 + body + html2, "html")
+        message.attach(m)
+        server.sendmail(
+            sender_email, receiver_email[0][0], message.as_string()
+        )
+        print('message sent to: %s' %a)
 
 # send_an_email("username", "email")
