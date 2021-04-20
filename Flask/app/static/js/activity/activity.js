f7a3701387c43a4f08074909dde40ec6954086d3
Phillip Zoghbi <phillip.zoghby@gmail.com>
2021-04-20 23:13:00 +0200

    Suggestions For Access -> Improvement -> Single Request

--- Flask/app/static/js/activity/activity.js
+++ Flask/app/static/js/activity/activity.js
@@ -897,6 +897,7 @@ function openDate() {
     }
 }
 
+var users = [];
 function GetUsers(element) {
     // Get The Input Box Text
     let input = element.value;
@@ -907,69 +908,71 @@ function GetUsers(element) {
         return;
     }
 
-    // Make A Request
-    $.ajax({
-        url: '/get_all_users',
-        method: 'GET',
-        timeout: 10000,
-        success: function(data) {
-            let users = JSON.parse(data)['users'];
-            if (!users) return;
-
-            let user_list = [];
-            for (user of users) {
-                // Add User To "The List"
-                if (user.startsWith(input)) {
-                    user_list.push(user);
+    // Make A Request If Array Is Empty
+    if (!users.length) {
+        $.ajax({
+            url: '/get_all_users',
+            method: 'GET',
+            timeout: 10000,
+            success: function(data) {
+                users = JSON.parse(data)['users'];
+                CreateSuggestionDropdown();
+            },
+            error: function($jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown + ": " + $jqXHR.responseText);
+                MakeSnackbar($jqXHR.responseText);
+                if ($jqXHR.status == 401) {
+                    // Handle Failure
                 }
             }
-            
-            // Remove Any Existing
-            ClearInput();
+        });
+    // If Users Are Fetched - Make A Dropdown
+    } else {
+        CreateSuggestionDropdown();
+    }
 
-            // Create & Style Dropdown Element
-            let dropdown = document.createElement("DIV");
-            dropdown.id = "autocomplete-dropdown";
-
-            // Iterate Through Results And Create Item For Each
-            user_list.forEach((user) => {
-                let item = document.createElement("DIV");
-                item.className = "autocomplete-result";
-
-                // Add Text To The Item
-                let name = document.createElement("SPAN");
-                name.textContent = user;
-                item.appendChild(name);
-
-                // Autocomplete
-                item.onclick = function() {
-                    element.value = user;
-                    ClearInput();
-                }
-                
-                // Navigate Through Options
-                    // ... 
-
-                // Add Item To Dropdown
-                dropdown.appendChild(item);
-            });
-
-            // Add Dropdown To The Input Group
-            element.parentElement.appendChild(dropdown);
-
-            // Debug
-            console.log(users);
-            console.log(user_list);
-            console.log(dropdown);
-        },
-        error: function($jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown + ": " + $jqXHR.responseText);
-            MakeSnackbar($jqXHR.responseText);
-            if ($jqXHR.status == 401) {
-                // Handle Failure
+    function CreateSuggestionDropdown() {
+        let user_list = [];
+        for (user of users) {
+            // Add User To "The List"
+            if (user.startsWith(input)) {
+                user_list.push(user);
             }
         }
-    });
+        
+        // Remove Any Existing
+        ClearInput();
+
+        // Create & Style Dropdown Element
+        let dropdown = document.createElement("DIV");
+        dropdown.id = "autocomplete-dropdown";
+
+        // Iterate Through Results And Create Item For Each
+        user_list.forEach((user) => {
+            let item = document.createElement("DIV");
+            item.className = "autocomplete-result";
+
+            // Add Text To The Item
+            let name = document.createElement("SPAN");
+            name.textContent = user;
+            item.appendChild(name);
+
+            // Autocomplete
+            item.onclick = function() {
+                element.value = user;
+                ClearInput();
+            }
+            
+            // Navigate Through Options
+                // ... 
+
+            // Add Item To Dropdown
+            dropdown.appendChild(item);
+        });
+
+        // Add Dropdown To The Input Group
+        element.parentElement.appendChild(dropdown);
+    }
 
     function ClearInput() {
         // Remove Any Existing
